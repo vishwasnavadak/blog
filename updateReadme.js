cd4bd028417a4f3dd065ac78e6e90bb25082576f
Vishwas Navada <navadavishwas@hotmail.com>
2019-02-27 23:15:53 +0530

    node script to update README.md when new post is published

---
+++ updateReadme.js
@@ -0,0 +1,19 @@
+const { items: posts } = require("./_site/feed.json");
+const insertLine = require("insert-line");
+
+insertLine("./README.md")
+  .content(
+    `#### ${posts[0].date_published.substring(0, 10)} - [${
+      posts[0].title
+    }](https://vishwas.tech/${posts[0].id.substring(
+      22,
+      posts[0].id.length
+    )}?utm_source=GitHub&utm_medium=GitHubRepo&utm_campaign=GitHubTraffic) \r\n`
+  )
+  .at(3)
+  .then(err => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("README.md updated with new post!");
+  });
